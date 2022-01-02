0c896829e64b5b024512b4d651dfdfd93c881f96
Никита Куваев <likeaboss13371@gmail.com>
2022-01-02 15:37:01 +0200

    Invoice, Order & Product Services Added

--- e-commerce/src/app/services/order/order.service.ts
+++ e-commerce/src/app/services/order/order.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {ErrorHandler} from "../../shared/error-handler";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Order} from "../../models/order";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
+  private errorHandler: ErrorHandler = new ErrorHandler();
+  private orderUrl = 'http://localhost:4200/orders/user-orders';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  // для админа
+  getOrders(): Observable<Order[]> {
+    try {
+      return this.http.get<Order[]>(this.orderUrl);
+    } catch (error) {
+      this.errorHandler.handleError(error);
+    }
+  }
+
+  // для юзера
+  getUserOrder(orderId: number): Observable<Order> {
+    try {
+      return this.http.get<Order>(`${this.orderUrl}/${orderId}`);
+    } catch (error) {
+      this.errorHandler.handleError(error);
+    }
+  }
 }
